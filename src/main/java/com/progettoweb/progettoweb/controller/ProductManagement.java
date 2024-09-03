f272b98553648ce9b5bca9e434a47b92cb2aec23
Al3fabbrii <alessandr04.fabbri@edu.unife.it>
2024-09-03 12:16:09 +0200

    Creata gestione dei prodotti

--- src/main/java/com/progettoweb/progettoweb/controller/ProductManagement.java
+++ src/main/java/com/progettoweb/progettoweb/controller/ProductManagement.java
@@ -212,7 +212,7 @@ public class ProductManagement extends HttpServlet {
             int avalaibility = Integer.parseInt(request.getParameter("Quantità"));
 
             String photo = request.getParameter("img_path");
-            //se la foto non è inserita metto di deafault questa
+            //se la foto non è inserita metto di deafault quest
             if(photo.isEmpty()){
                 photo = "https://media.istockphoto.com/id/1472933890/vector/no-image-vector-symbol-missing-available-icon-no-gallery-for-this-moment-placeholder.jpg?s=612x612&w=0&k=20&c=Rdn-lecwAj8ciQEccm0Ep2RX50FCuUJOaEM8qQjiLL0=" ;
             }
